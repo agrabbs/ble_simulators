3b58c9db0f2979f16fbde46ab3e4e63e28c53991
Andrew Grabbs <agrabbs@gmail.com>
2022-08-15 08:19:12 -0500

    Fixed rebroadcasting issues after disconnect

--- wahoo_tickr_mock_esp32.cpp
+++ wahoo_tickr_mock_esp32.cpp
@@ -1,27 +1,9 @@
+#include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <BLEUUID.h>
-
-BLEServer *pServer = NULL;
-BLECharacteristic * batt_level;
-BLECharacteristic * body_sensor_loc;
-BLECharacteristic * hr_measure;
-BLECharacteristic * mfg;
-BLECharacteristic * sns;
-BLECharacteristic * hrs;
-BLECharacteristic * frs;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t hr[] = { 0x16, 0x63, 0x83, 0x02 };
-uint8_t batt = 0x64;
-uint8_t sensor_loc = 0x01;
-uint8_t mfg_name[] = { 0x57, 0x61, 0x68, 0x6f, 0x6f, 0x20, 0x46, 0x69, 0x74, 0x6e, 0x65, 0x73, 0x73 }; // Wahoo Fitness
-uint8_t ser_str[] = { 0x32, 0x30, 0x30, 0x36, 0x30, 0x30, 0x31, 0x39, 0x36 };
-uint8_t hw_str[] = { 0x32, 0x31 };
-uint8_t fw_str[] = { 0x31, 0x2e, 0x39, 0x2e, 0x32 };
-uint16_t appearance = 0x0340;
 
 #define NAME "TICKR MOCK1"
 #define DEV_INFO_UUID "0000180a-0000-1000-8000-00805f9b34fb"
@@ -35,13 +17,36 @@ uint16_t appearance = 0x0340;
 #define BATT_SVC_UUID "0000180f-0000-1000-8000-00805f9b34fb"
 #define BATT_LVL "00002a19-0000-1000-8000-00805f9b34fb"
 
+BLEServer *pServer = NULL;
+BLECharacteristic *batt_level;
+BLECharacteristic *body_sensor_loc;
+BLECharacteristic *hr_measure;
+BLECharacteristic *mfg;
+BLECharacteristic *sns;
+BLECharacteristic *hrs;
+BLECharacteristic *frs;
+
+bool deviceConnected = false;
+
+uint8_t hr[] = { 0x16, 0x63, 0x83, 0x02 };
+uint8_t batt = 0x64;
+uint8_t sensor_loc = 0x01;
+uint8_t mfg_name[] = { 0x57, 0x61, 0x68, 0x6f, 0x6f, 0x20, 0x46, 0x69, 0x74, 0x6e, 0x65, 0x73, 0x73 }; // Wahoo Fitness
+uint8_t ser_str[] = { 0x32, 0x30, 0x30, 0x36, 0x30, 0x30, 0x31, 0x39, 0x36 };
+uint8_t hw_str[] = { 0x32, 0x31 };
+uint8_t fw_str[] = { 0x31, 0x2e, 0x39, 0x2e, 0x32 };
+uint16_t appearance = 0x0340;
+
 class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
+    void onConnect(BLEServer *pServer) {
       deviceConnected = true;
+      Serial.println("Client connected");
     };
 
-    void onDisconnect(BLEServer* pServer) {
+    void onDisconnect(BLEServer *pServer) {
       deviceConnected = false;
+      Serial.println("Client disconnected");
+      BLEDevice::startAdvertising();
     }
 };
 
@@ -56,9 +61,9 @@ void setup() {
   pServer->setCallbacks(new MyServerCallbacks());
   
   // Create the BLE Services
-  BLEService * pService = pServer->createService(HR_SVC_UUID);
-  BLEService * pService2 = pServer->createService(BATT_SVC_UUID);
-  BLEService * pService3 = pServer->createService(DEV_INFO_UUID);
+  BLEService *pService = pServer->createService(HR_SVC_UUID);
+  BLEService *pService2 = pServer->createService(BATT_SVC_UUID);
+  BLEService *pService3 = pServer->createService(DEV_INFO_UUID);
   
   // Battery Level Service Chars
   batt_level = pService2->createCharacteristic(
@@ -91,7 +96,7 @@ void setup() {
   frs = pService3->createCharacteristic(FW_REV_STR, BLECharacteristic::PROPERTY_READ);
   frs->setValue((uint8_t*)&fw_str, 5);
 
-  // Start the service
+  // Start the services
   pService2->start();
   pService3->start();
   pService->start();
@@ -109,34 +114,20 @@ void setup() {
 }
 
 void loop() {
-
-    if (deviceConnected) {
-      while(1) {
-        for (int i = 60; i <= 200; i++) {
-          hr[1] = i;
-          hr_measure->setValue((uint8_t*)& hr, 4);
-          hr_measure->notify();
-          delay(1000);
-        }
-        for (int i = 200; i >= 60; i--) {
-          hr[1] = i;
-          hr_measure->setValue((uint8_t*)& hr, 4);
-          hr_measure->notify();
-          delay(1000);
-        }
+  if (deviceConnected) {
+    while(1) {
+      for (int i = 60; i <= 200; i++) {
+        hr[1] = i;
+        hr_measure->setValue((uint8_t*)& hr, 4);
+        hr_measure->notify();
+        delay(1000);
       }
+      for (int i = 200; i >= 60; i--) {
+        hr[1] = i;
+        hr_measure->setValue((uint8_t*)& hr, 4);
+        hr_measure->notify();
+        delay(1000);
+      }
+    }
   }
-
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-    // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
 }
